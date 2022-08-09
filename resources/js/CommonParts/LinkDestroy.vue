4b30a30025e1d61c929845a729445d1e684be6d1
HC Torres <hctorres02@gmail.com>
2022-08-09 04:14:45 -0300

    Adiciona LindDestroy

---
+++ resources/js/CommonParts/LinkDestroy.vue
@@ -0,0 +1,21 @@
+<template>
+    <Link :href="href" as="button" method="delete" class="btn btn-danger">
+        <i class="bi bi-trash"></i>
+        {{ label }}
+    </Link>
+</template>
+
+<script>
+    import { Link } from "@inertiajs/inertia-vue";
+
+    export default {
+        name: "LinkDestroy",
+        props: {
+            href: String,
+            label: String
+        },
+        components: {
+            Link
+        }
+    };
+</script>
